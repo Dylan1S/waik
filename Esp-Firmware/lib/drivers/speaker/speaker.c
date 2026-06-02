0fc6ecc10102db315f82045e0313a161c62e0036
Dylan1S <22dylan.saayman@gmail.com>
2026-06-03 00:46:20 +0200

    added: speaker driver implementation in speaker.c

---
+++ Esp-Firmware/lib/drivers/speaker/speaker.c
@@ -0,0 +1,74 @@
+#include "speaker/include/speaker.h"
+
+static i2s_chan_handle_t tx_handle;
+static const char *SPK_TAG = "Speaker: ";
+
+//function implementations
+
+esp_err_t spk_init(void)
+{
+    i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_1, I2S_ROLE_SLAVE);
+
+    ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, tx_handle, NULL));
+
+    i2s_std_config_t std_cfg = {
+        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(SAMPLE_RATE),
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(
+            I2S_DATA_BIT_WIDTH_16BIT,
+            I2S_SLOT_MODE_MONO
+        ),
+        .gpio_cfg = {
+            .mclk   = I2S_GPIO_UNUSED,
+            .bclk   = SPK_I2S_BCK_PIN,
+            .ws     = SPK_I2S_LRC_PIN,
+            .dout   = I2S_GPIO_UNUSED,
+            .din    = SPK_I2S_DATA_PIN,
+            .invert_flags = {
+                .mclk_inv = false,
+                .bclk_inv = false,
+                .ws_inv   = false
+            }
+        }
+    };
+
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(tx_handle, &std_cfg));
+    #ifdef DEBUG
+        ESP_LOGI(SPK_TAG, "I2S initialized - BCK: %d, WS: %d, DOUT: %d",
+        SPK_I2S_BCK_PIN, SPK_I2S_LRC_PIN, SPK_I2S_DATA_PIN);
+    #endif
+}
+
+
+/*Enable the mic's I2S Channel. Mic must be initialized*/
+esp_err_t spk_enable(void)
+{
+    ESP_ERROR_CHECK(i2s_channel_enable(tx_handle));
+}
+/*Disable the mic but don't delete it's I2S Channel*/
+esp_err_t spk_disable(void)
+{
+    ESP_ERROR_CHECK(i2s_channel_disable(tx_handle));
+}
+
+/*Delete the mic's I2S Channel*/
+esp_err_t spk_deinit(void)
+{
+    ESP_ERR_CHECK(i2s_del_channel(tx_handle));
+}
+
+esp_err_t spk_write(int16_t *buf, size_t bytes_to_write, size_t *bytes_read, uint32_t timeout_ms)
+{
+    esp_err_t ret = i2s_channel_write(
+        tx_handle,
+        buf,
+        sizeof(buf),
+        bytes_read,
+        pdMS_TO_TICKS(timeout_ms)
+    );
+    if (ret == ESP_OK && bytes_read > 0) {
+        size_t samples_read = *bytes_read/sizeof(int16_t);
+        #ifdef DEBUG
+            ESP_LOGW(SPK_TAG, "I2S write failed or timed out: %s", esp_err_to_name(ret));
+        #endif
+    }
+}
