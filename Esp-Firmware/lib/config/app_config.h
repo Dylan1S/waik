6dc7a9ce51007e2948ede1762a4c437b1fb3cfed
Dylan1S <22dylan.saayman@gmail.com>
2026-06-02 21:32:12 +0200

    add: config files with global macros in lib/config. updated: project structure

---
+++ Esp-Firmware/lib/config/app_config.h
@@ -0,0 +1,9 @@
+#ifndef APP_CFG_H
+#define APP_CFG_H
+
+#define SAMPLE_RATE     16000
+#define BUFFER_SIZE     512
+#define DEBUG
+
+
+#endif
---
+++ Esp-Firmware/lib/config/board.h
@@ -0,0 +1,19 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <driver/gpio.h>
+
+
+//These are the pin definitions for the INMP441 Microphone
+#define MIC_I2S_BCK_PIN     GPIO_NUM_8
+#define MIC_I2S_WS_PIN      GPIO_NUM_21
+#define MIC_I2S_DATA_PIN    GPIO_NUM_47
+
+
+//These are the pin definitions for the MAX98357A amp
+#define SPKR_I2S_BCK_PIN    GPIO_NUM_38
+#define SPKR_I2S_LRC_PIN    GPIO_NUM_38
+#define SPKR_I2S_DATA_PIN   GPIO_NUM_1
+
+
+#endif
