d599a80487ec1f7a344148d18c9c6acb0cace82a
Dylan1S <22dylan.saayman@gmail.com>
2026-06-04 23:03:02 +0200

    added: minor adjustments to audio_pipeline

--- Esp-Firmware/lib/audio/audio_pipeline.c
+++ Esp-Firmware/lib/audio/audio_pipeline.c
@@ -1,1 +1,42 @@
 #include "audio_pipeline.h"
+#include "esp_aec.h"
+#include "esp_afe_config.h"
+#include "esp_afe_sr_iface.h"
+
+
+static const esp_afe_sr_iface_t *afe_handle = NULL;
+
+
+
+typedef struct audio_pipeline_obj_t
+{
+    TaskHandle_t    capture_task;
+    QueueHandle_t   audio_queue;
+    bool            is_running;
+}audio_pipeline_obj_t;
+
+
+
+esp_err_t audio_pipeline_init(void)
+{
+    srmodel_list_t* models = esp_srmodel_init("model");
+    afe_config_t* afe_config = afe_config_init("MR", models, AFE_TYPE_SR, AFE_MODE_HIGH_PERF);
+
+    
+}
+
+esp_err_t audio_pipeline_begin(void)
+{
+
+}
+
+
+esp_err_t audio_pipeline_reset(void)
+{
+
+}
+
+esp_err_t audio_pipeline_disable(void)
+{
+
+}
--- Esp-Firmware/lib/audio/include/audio_pipeline.h
+++ Esp-Firmware/lib/audio/include/audio_pipeline.h
@@ -1,9 +1,22 @@
-#ifndef _AUD_PIP_H_
-#define _AUD_PIP_H_
+#ifndef _AUDIO_PIPE_H_
+#define _AUDIO_PIPE_H_
 #include "board.h"
 #include "app_config.h"
 #include "speaker.h"
 #include "mic.h"
+#include "freertos/FreeRTOS.h"
+
+typedef struct audio_pipeline_obj_t* audio_pipeline_handle_t;
+
+esp_err_t audio_pipeline_init(void);
+
+esp_err_t audio_pipeline_begin();
+
+esp_err_t audio_pipeline_reset();
+
+esp_err_t audio_pipeline_disable(void);
+
+
 
 
 
