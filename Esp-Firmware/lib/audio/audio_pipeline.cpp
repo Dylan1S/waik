e8a19d6e22a4a901deabdde7ee58a814e0188ea4
Dylan1S <22dylan.saayman@gmail.com>
2026-06-04 23:59:55 +0200

    refactor: changed audio_pipeline.c to audio_pipeline.cpp

--- Esp-Firmware/lib/audio/audio_pipeline.cpp
+++ Esp-Firmware/lib/audio/audio_pipeline.cpp
@@ -1,28 +1,22 @@
 #include "audio_pipeline.h"
+#include "board.h"
+#include "app_config.h"
+#include "speaker.h"
+#include "mic.h"
+#include "freertos/FreeRTOS.h"
 #include "esp_aec.h"
 #include "esp_afe_config.h"
 #include "esp_afe_sr_iface.h"
 
+namespace{
+    const esp_afe_sr_iface_t *afe_handle = NULL;
 
-static const esp_afe_sr_iface_t *afe_handle = NULL;
-
-
-
-typedef struct audio_pipeline_obj_t
-{
-    TaskHandle_t    capture_task;
-    QueueHandle_t   audio_queue;
-    bool            is_running;
-}audio_pipeline_obj_t;
-
-
+}
 
 esp_err_t audio_pipeline_init(void)
 {
     srmodel_list_t* models = esp_srmodel_init("model");
-    afe_config_t* afe_config = afe_config_init("MR", models, AFE_TYPE_SR, AFE_MODE_HIGH_PERF);
-
-    
+    afe_config_t* afe_config = afe_config_init("MR", models, AFE_TYPE_SR, AFE_MODE_HIGH_PERF);  
 }
 
 esp_err_t audio_pipeline_begin(void)
--- Esp-Firmware/lib/audio/include/audio_pipeline.h
+++ Esp-Firmware/lib/audio/include/audio_pipeline.h
@@ -1,22 +1,21 @@
 #ifndef _AUDIO_PIPE_H_
 #define _AUDIO_PIPE_H_
-#include "board.h"
-#include "app_config.h"
-#include "speaker.h"
-#include "mic.h"
-#include "freertos/FreeRTOS.h"
-
-typedef struct audio_pipeline_obj_t* audio_pipeline_handle_t;
-
-esp_err_t audio_pipeline_init(void);
-
-esp_err_t audio_pipeline_begin();
-
-esp_err_t audio_pipeline_reset();
-
-esp_err_t audio_pipeline_disable(void);
 
 
+
+class AudioPipeline
+{
+public:
+    esp_err_t init();
+    esp_err_t begin();
+    esp_err_t reset();
+    esp_err_t disable();
+
+private:
+    TaskHandle_t    m_capture_task = nullptr;
+    QueueHandle_t   m_audio_queue = nullptr;
+    bool            m_is_running = false;
+};
 
 
 
