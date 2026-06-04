8026ec77523919ccd13d9403523674287fd895f8
Dylan <symdyl001@myuct.ac.za>
2026-06-04 20:32:29 +0200

    updated: added return type ESP_OK to all functions

--- Esp-Firmware/lib/drivers/speaker/speaker.c
+++ Esp-Firmware/lib/drivers/speaker/speaker.c
@@ -1,4 +1,4 @@
-#include "speaker/include/speaker.h"
+#include "speaker.h"
 
 static i2s_chan_handle_t tx_handle;
 static const char *SPK_TAG = "Speaker: ";
@@ -9,7 +9,7 @@ esp_err_t spk_init(void)
 {
     i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_1, I2S_ROLE_SLAVE);
 
-    ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, tx_handle, NULL));
+    ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, &tx_handle, NULL));
 
     i2s_std_config_t std_cfg = {
         .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(SAMPLE_RATE),
@@ -36,6 +36,8 @@ esp_err_t spk_init(void)
         ESP_LOGI(SPK_TAG, "I2S initialized - BCK: %d, WS: %d, DOUT: %d",
         SPK_I2S_BCK_PIN, SPK_I2S_LRC_PIN, SPK_I2S_DATA_PIN);
     #endif
+
+    return ESP_OK;
 }
 
 
@@ -43,17 +45,23 @@ esp_err_t spk_init(void)
 esp_err_t spk_enable(void)
 {
     ESP_ERROR_CHECK(i2s_channel_enable(tx_handle));
+    
+    return ESP_OK;
 }
 /*Disable the mic but don't delete it's I2S Channel*/
 esp_err_t spk_disable(void)
 {
     ESP_ERROR_CHECK(i2s_channel_disable(tx_handle));
+    
+    return ESP_OK;
 }
 
 /*Delete the mic's I2S Channel*/
 esp_err_t spk_deinit(void)
 {
-    ESP_ERR_CHECK(i2s_del_channel(tx_handle));
+    ESP_ERROR_CHECK(i2s_del_channel(tx_handle));
+    
+    return ESP_OK;
 }
 
 esp_err_t spk_write(int16_t *buf, size_t bytes_to_write, size_t *bytes_read, uint32_t timeout_ms)
@@ -71,4 +79,6 @@ esp_err_t spk_write(int16_t *buf, size_t bytes_to_write, size_t *bytes_read, uin
             ESP_LOGW(SPK_TAG, "I2S write failed or timed out: %s", esp_err_to_name(ret));
         #endif
     }
+    
+    return ESP_OK;
 }
