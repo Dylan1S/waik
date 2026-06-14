5c06c1ebccd008b5102465b13308d11250bf96da
Dylan1S <22dylan.saayman@gmail.com>
2026-06-14 23:04:20 +0200

    added: vad config to audio_pipeline

--- Esp-Firmware/lib/audio/audio_pipeline.cpp
+++ Esp-Firmware/lib/audio/audio_pipeline.cpp
@@ -3,11 +3,43 @@
 #include "app_config.h"
 #include "speaker.h"
 #include "mic.h"
+
 #include "esp_aec.h"
 #include "esp_afe_config.h"
 #include "esp_afe_sr_iface.h"
+#include "esp_afe_aec.h"
+#include "esp_afe_sr_models.h"
+#include "esp_vad.h"
+#include "esp_log.h"
+
 
 namespace{
+    const char* TAG {"AudioPipeline"};
+
+    void configure_vad(afe_config_t* config)
+    {
+        if(config == nullptr)
+        {
+            ESP_LOGE(TAG, "can't configure VAD, afe_config is nullptr");
+            return;
+        }
+        config->vad_init = true;
+        config->vad_min_noise_ms = 1024;
+        config->vad_min_speech_ms = 128;
+        config->vad_delay_ms = 128;
+        config->vad_mode = VAD_MODE_1;
+    }
+
+    void configure_aec(afe_config_t* config)
+    {
+        if(config == nullptr)
+        {
+            ESP_LOGE(TAG, "can't configure AEC, afe_config is nullptr");
+            return;
+        }
+    }
+
+
 
 
 }
@@ -15,7 +47,27 @@ namespace{
 esp_err_t AudioPipeline::init()
 {
     srmodel_list_t* models = esp_srmodel_init("model");
-    afe_config_t* afe_config = afe_config_init("MR", models, AFE_TYPE_SR, AFE_MODE_HIGH_PERF);  
+    if(!models){
+         ESP_LOGE(TAG, "Failed to load SR models");
+         return ESP_FAIL;
+    }
+
+    afe_config_t* afe_config = afe_config_init("MR", models, AFE_TYPE_SR, AFE_MODE_HIGH_PERF);
+    if(!afe_config){
+        ESP_LOGE(TAG, "Failed to create AFE config");
+        return ESP_FAIL;
+    }
+
+    configure_vad(afe_config);
+
+    const esp_afe_sr_iface_t *afe_handle = esp_afe_handle_from_config(afe_config);
+    if(!afe_handle){
+        ESP_LOGE(TAG, "Failed to create AFE handle from AFE config");
+        return ESP_FAIL;
+    }
+    
+    esp_afe_sr_data_t *afe_data = afe_handle->create_from_config(afe_config);
+    
 
     return ESP_OK;
 }
