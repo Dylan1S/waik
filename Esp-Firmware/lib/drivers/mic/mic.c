ab24a8d0e3e03d226dfd6ba9dbc4aca7affeecfb
Dylan1S <22dylan.saayman@gmail.com>
2026-06-15 01:06:09 +0200

    fix: logging is less strict to prevent cluttering monitor when a failure happens

--- Esp-Firmware/lib/drivers/mic/mic.c
+++ Esp-Firmware/lib/drivers/mic/mic.c
@@ -47,7 +47,7 @@ esp_err_t mic_init(void)
     //initializing I2S channel
     ESP_ERROR_CHECK(i2s_channel_init_std_mode(rx_handle, &std_cfg));
     ESP_LOGI(
-        MIC_TAG, "I2S initialized - BCK: %d, WS: %d, DIN %D",
+        MIC_TAG, "I2S initialized - BCK: %d, WS: %d, DIN %d",
         MIC_I2S_BCK_PIN, MIC_I2S_WS_PIN, MIC_I2S_DATA_PIN
     );
     return ESP_OK;
@@ -75,20 +75,21 @@ esp_err_t mic_deinit(void)
     return ESP_OK;
 }
 
-esp_err_t mic_read(int16_t *buf, size_t num_samples, size_t *bytes_read, uint32_t timeout_ms)
+esp_err_t mic_read(int16_t *buf, size_t num_samples, size_t* bytes_read, uint32_t timeout_ms)
 {
+    size_t bytes_to_read = num_samples * sizeof(int16_t);
     esp_err_t ret = i2s_channel_read(
         rx_handle,
         buf,
-        sizeof(buf),
+        bytes_to_read,
         bytes_read,
         pdMS_TO_TICKS(timeout_ms)
     );
     if (ret == ESP_OK && bytes_read > 0) {
         size_t samples_read = *bytes_read/sizeof(int16_t);
-        #ifdef DEBUG
-            ESP_LOGW(MIC_TAG, "I2S read failed or timed out: %s", esp_err_to_name(ret));
-        #endif
+    }
+    else {
+        ESP_LOGW(MIC_TAG, "I2S read failed or timed out: %s", esp_err_to_name(ret));
     }
 
     return ESP_OK;
